0d387589d29341e7c55cd59e1754fd3802bacfcd
Gift Mbonani <giftmbonani@outlook.com>
2023-07-17 14:07:25 -0700

    task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,13 @@
 #include<stdio.h>
+
 /**
  * main - this is the main function
  *
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-	printf("with proper grammer, but the outcome is a piece of art,");
+	printf("with proper grammer, but the outcome is a piece of art,\n");
 	return (0);
 }
