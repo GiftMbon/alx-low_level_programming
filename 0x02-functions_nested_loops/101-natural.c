60c6794655862f16254122776046bc9e1fbcd857
Gift Mbonani <giftmbonani@outlook.com>
2023-07-19 15:53:12 -0700

    task 13

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -2,7 +2,7 @@
 
 /**
  * main - prints the sum of all multiples of 3 or 5 up to 1024
- * Return: Always (Success)
+ * Return: Always 0 (Success)
  */
 
 int main(void)
@@ -12,7 +12,7 @@ int main(void)
 
 	while (i < 1024)
 	{
-		if ((i % 3 == %) || (i % 5 == 0))
+		if ((i % 3 == 0) || (i % 5 == 0))
 		{
 			z += i;
 		}
